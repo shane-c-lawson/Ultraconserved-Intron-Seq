8e6cc9baa88dedc70c3194a991db3bd0fd79519c
shane-c-lawson <sclsoxfan017@charter.net>
2023-10-06 10:45:18 -0400

    Delete eCLIP and Motif Analysis directory

--- eCLIP and Motif Analysis/pie_chart_distal_ints.R
+++
@@ -1,8 +0,0 @@
-slices <- c((2364-473-89-160-227-2-3), 473, (89+227+2), (160+3))
-lbls <- c("Other", "Start/Stop Exon", "Cons_NMD_In", "Cons_NMD_Ex")
-pct <- round(slices/sum(slices)*100, 1)
-#lbls <- paste(lbls, pct) # add percents to labels
-#lbls <- paste(lbls,"%",sep="") # ad % to labels
-cols = c('lightblue', 'orange', "indianred", "seagreen")
-pie(slices,labels = lbls, col=cols,#rainbow(length(lbls)),
-    main="Exons with distant intron conservation", cex.main = 1)#, font.family='Arial')+
